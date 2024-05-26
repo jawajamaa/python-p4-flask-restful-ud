8ff36ec6454f4cd31a4ccbfe468e822634f6a718
Timothy Ryon <tim@timryon.com>
2024-05-26 13:01:26 -0400

    finish codealong with a couple adjustments

--- server/app.py
+++ server/app.py
@@ -69,6 +69,8 @@ api.add_resource(Newsletters, '/newsletters')
 
 class NewsletterByID(Resource):
 
+    # add validation to check for letter == None?
+
     def get(self, id):
 
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
@@ -79,7 +81,36 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
 
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        letter_dict = record.to_dict()
+        return make_response( letter_dict, 202 )
+    # above return is more succinct than creating response variable, then returning it on a separate line, and the HTTP code is different from what what the example was, but I think (and more importantly, from the instructor's lectures, 202 is more accurate?) 
+
+    def delete(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+        response_body = {
+            "Message": "Record successfully deleted"
+        }
+        # below is my longer message, so not necessary?
+        # response_body = {
+        #     "Delete Successful!!": True,
+        #     "Message": "Record deleted"
+        # }
+        return make_response( response_body, 202)
+    # above return is more succinct than creating response variable, then returning it on a separate line, and the HTTP code is different from what what the example was, but I think (and more importantly, from the instructor's lectures, 202 is more accurate?) 
+    
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 
